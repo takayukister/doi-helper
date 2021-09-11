6c962b8e0fdf7ea8028658e1fe630499742a0a68
Takayuki Miyoshi <takayukister@gmail.com>
2021-09-11 15:12:59 +0900

    Register doihelper_entry post type

--- doi-helper.php
+++ doi-helper.php
@@ -13,3 +13,22 @@ Version: 0.72
 define( 'DOIHELPER_VERSION', '0.72' );
 
 define( 'DOIHELPER_PLUGIN', __FILE__ );
+
+
+add_action( 'init',
+	function () {
+		register_post_type(
+			'doihelper_entry',
+			array(
+				'labels' => array(
+					'name' => __( 'DOI Entries', 'doi-helper' ),
+					'singular_name' => __( 'DOI Entry', 'doi-helper' ),
+				),
+				'public' => false,
+				'rewrite' => false,
+				'query_var' => false,
+			)
+		);
+	},
+	10, 0
+);
