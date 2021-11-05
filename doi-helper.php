789e8281a68fb96ed85cecb575bebeecdd37e0a6
Takayuki Miyoshi <takayukister@gmail.com>
2021-11-05 16:18:46 +0900

    For packaging v0.73

--- doi-helper.php
+++ doi-helper.php
@@ -7,10 +7,10 @@ Author: Takayuki Miyoshi
 Author URI: https://ideasilo.wordpress.com/
 Text Domain: doi-helper
 Domain Path: /languages/
-Version: 0.72
+Version: 0.73
 */
 
-define( 'DOIHELPER_VERSION', '0.72' );
+define( 'DOIHELPER_VERSION', '0.73' );
 
 define( 'DOIHELPER_PLUGIN', __FILE__ );
 
