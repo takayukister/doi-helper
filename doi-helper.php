485a1e94f0ccf5c0f2ddc434e8dd9c1d2e79cb56
Takayuki Miyoshi <takayukister@gmail.com>
2024-05-07 17:30:09 +0900

    Update doi-helper.php

--- doi-helper.php
+++ doi-helper.php
@@ -8,7 +8,7 @@
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Version: 1.0
- * Requires at least: 6.1
+ * Requires at least: 6.4
  * Requires PHP: 7.4
  */
 
